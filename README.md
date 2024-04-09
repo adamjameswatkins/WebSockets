d7808cc2eecfe898fe0610dc92e5112891f094d0
AdamJamesWatkins <adamjwatkins@gmail.com>
2024-04-09 23:53:19 +0100

    Experiment with websockets

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple WebSockets app
+```shell
+cd .\server\
+
+npm init -y
+
+npm i ws
+```
+
+`ws://localhost:8082` is equivalent to `wss://localhost:8082` but the latter is the secure version
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.on('connection', ws => {
+    console.log('Client connected');
+
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`);
+
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
